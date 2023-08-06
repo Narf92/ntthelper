f77e62baedc96f60462bf8a90d932584cb4d9686
Francisco Martinez <fmartinez92@gmail.com>
2023-08-06 18:37:02 +0200

    fix deploy problem

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,6 +1,5 @@
 export { default as Buttons } from "./buttons/Buttons";
 export { default as UtilityCard } from "./cards/UtilityCard";
-export { default as Navbar } from "./navbar/Navbar";
 export { default as BCheckerSpace } from "./spaces/BCheckerSpace";
 export { default as Spaces } from "./spaces/Spaces";
 export { default as UTableSpace } from "./spaces/UTableSpace";
